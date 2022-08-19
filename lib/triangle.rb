97f204c7e9f6a1ddc623bf2e8cc29d7f0497bc64
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-08-19 14:09:29 +0300

    DONE

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  
+  def initialize(length1, length2,length3)
+
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  def kind
+    triangle = [@length1,@length2,@length3].uniq
+    pp triangle
+    if (@length1 > 0 && @length2 > 0 && @length3 > 0) && ((@length1 + @length2 > @length3) && (@length1 + @length3 > @length2 ) && (@length2 + @length3 > @length1))
+      if triangle.size == 1
+        :equilateral
+      elsif triangle.size == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+
+  end
+
+  class TriangleError < StandardError
+  end
+  
 end
+eq = Triangle.new(3,3,3)
